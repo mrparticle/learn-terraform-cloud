99d0bc83fdb4f3702d4806bb60f30c1c56cb62b1
mrparticle <49153240+mrparticle@users.noreply.github.com>
2022-06-30 15:48:37 +0100

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "Harley-GB"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
